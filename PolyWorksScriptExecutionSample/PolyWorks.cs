0e2d7f5d790cc9de082a49fbc8a5b87d7a6e6bae
Олег Трушков <oleg_trushkov@hotmail.com>
2021-08-26 16:41:45 +0300

    Add project files.

---
+++ PolyWorksScriptExecutionSample/PolyWorks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IMInspectLib;
+
+namespace PolyWorksScriptExecutionSample
+{
+    class PolyWorks
+    {
+        private IIMCommandCenter _imCommandCenter;
+        private IMInspect _imInspect;
+        private IIMInspectProject _imInspectProject;
+
+        public PolyWorks()
+        {
+            _imInspect = new IMInspect();
+            if (_imInspect == null) throw new Exception("Unable to connect to PolyWorks instance.");
+            _imInspect.ProjectGetCurrent(out _imInspectProject);
+            if (_imInspectProject == null) throw new Exception("Unable to get PolyWorks current project.");
+        }
+
+        public void ScriptExecuteFromFile(string script, string args = "")
+        {
+            _imCommandCenter = null;
+            _imInspectProject.CommandCenterCreate(out _imCommandCenter);
+            if (_imCommandCenter == null) throw new Exception("Unable to initialize PolyWorks command center.");
+            _imCommandCenter.ScriptExecuteFromFile(script, args);
+        }
+    }
+}
---
+++ PolyWorksScriptExecutionSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace PolyWorksScriptExecutionSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = Assembly.GetExecutingAssembly().Location;
+            var directory = Path.GetDirectoryName(path);
+            PolyWorks pw = new PolyWorks();
+            pw.ScriptExecuteFromFile(Path.Combine(directory, "PolyWorksScriptSample.pwmacro"));
+        }
+    }
+}
